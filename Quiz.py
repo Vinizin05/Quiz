a47a14c6c702d5a5179b8631cd020baa7eebaf80
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2022-11-23 14:04:36 -0300

    .

--- Quiz.py
+++ Quiz.py
@@ -1,5 +1,24 @@
 print("Bem Vindo ao meu Quiz")
- 
+answer_user = input("quer comecar? (S/N) ")
+
+if answer_user != "S":
+    quit()  
+    
+print("começando...")
+
+print("Para que serve o Github? \n (A) Para instalar jogos \n (B) Para Guardar seus codigos \n (C) Para ver videos")
+answer_1 = input("Resposta: ")
+
+if answer_1 == "B":
+    print("Resposta Correto")   
+else:
+    print("Resposta Incorreto")
+                                        
+if answer_1 != "B":
+    quit("Tente novamente...")
+        
+
+  
 
     
 
